77873719d4e5b48e642b26633c7c6d1c86921f07
KaiGeller <gellerka@oregonstate.edu>
2022-04-27 18:54:46 -0700

    1

--- Taxicab.py
+++ Taxicab.py
@@ -3,25 +3,25 @@
 #4/27/2022
 # Assignment number 5b this is to keep track of the x and y coordinate of the taxi and the odometer
 class Taxicab:
-    def __init__(self, x_cord, y_cord):
+    def __init__(self, x_coord, y_coord):
         """This defines the x and y coordinates of the cab and sets the odometer to 0"""
-        self.x_cord = x_cord
-        self.y_cord = y_cord
+        self.x_cord = x_coord
+        self.y_cord = y_coord
         self.odometer = 0
     def move_x(self, move_x):
         """This moves the cab along the x axis"""
         self.odometer += abs(move_x)
-        self.x_cord += move_x
+        self.x_coord += move_x
     def move_y(self, move_y):
        """This moves the cab along the y axis"""
        self.odometer += abs(move_y)
-       self.y_cord += move_y
+       self.y_coord += move_y
     def get_x_cord(self):
         """This gives current x coordinate of cab"""
-        return self.x_cord
+        return self.x_coord
     def get_y_cord(self):
         """This gives current x coordinate of cab"""
-        return self.y_cord
+        return self.y_coord
     def get_odometer(self):
         """This gives odometer value"""
         return self.odometer
